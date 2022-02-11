5cde5965d491b61355fe2790bf22b1fe149f2a36
Itamar Carvalho <itamarc@gmail.com>
2022-02-11 19:39:33 -0300

    Adding builder pattern to PetType on constructor
    Working on #54

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -15,11 +15,16 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Table(name = "types")
 public class PetType extends BaseEntity {
     @Column(name = "name")
     private String name;
+
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
 
     @Override
     public String toString() {
